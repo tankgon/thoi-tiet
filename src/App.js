28d95b8a3b218f1e2930b3cc96eb1f619c5d1fb1
tankgon <nguyenphucthanh1879@gmail.com>
2022-05-24 11:33:42 +0700

    done werther

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import axios from 'axios'
-import {useEffect,useState} from 'react'
+import {useEffect,useState, useRef} from 'react'
 const api = {
   key: "f940561a277d552c3b894777566e82f2",
   base: "https://api.openweathermap.org/data/2.5/",
@@ -13,7 +13,7 @@ function App() {
   const [listrender, setlistrender] = useState([]);
   const [Getname, setGetname] = useState("");
   const url = `${api.base}weather?q=${Getname}&units=metric&APPID=${api.key}`
-
+  const typingTimeoutRef = useRef(null);
 
 // ComponentDidMount
 
@@ -21,6 +21,7 @@ function App() {
     try{
       const a = await axios.get(url);
       setlistrender(a.data)
+      
     }
     catch (error){
       alert("loi api")
@@ -29,7 +30,18 @@ function App() {
   
   const onChageName = (e) => {
     setGetname(e.target.value)
+    if(typingTimeoutRef.current){
+      clearTimeout(typingTimeoutRef.current)
+    }
+    else{
+      typingTimeoutRef.current = setTimeout(()=>{
+        const formValue = {
+          Getname: e.target.value,
+        };
+      },5000)
+    }
   }
+
 
   
   const checkwerther = () =>{
@@ -40,19 +52,22 @@ function App() {
       alert("cold")
     }
   }
+  console.log(listrender?.rain);
   
 
   return (
     <div className="background margin">
 
       <form action="/action_page.php">
-      <input value={Getname} type="text" onChange={onChageName}/>
-      <p className='buttom' onClick={lookfor}>Tìm kiếm</p>
-      <p className='buttom' onClick={checkwerther}>check</p>
+      <input value={Getname} type="text"  onChange={onChageName}/>
+      <p className='buttom' onClick={lookfor}>Tim kiem</p>
+      {/* <p className='buttom' onClick={checkwerther}>check</p> */}
       </form>
         <div className='font'>Nhiệt độ là: { listrender?.main?.temp}<sup>0</sup>C</div>
         <div className='font'>Tên thành phố {listrender?.name}</div>
         <div className='font'>Tên quốc gia: {listrender?.sys?.country}</div>
+        {listrender?.rain ? <div>co mua lúc {listrender?.rain}</div> :<div>khong co mua</div>}
+
     </div>
   );
 }
