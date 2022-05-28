0c0053ba793ab1e0f9f37465fbad088edeacb6f8
tankgon <nguyenphucthanh1879@gmail.com>
2022-05-28 15:02:48 +0700

    done UX/UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react'
+import React, { useCallback } from 'react'
 import axios from 'axios'
 import {useEffect,useState, useRef} from 'react'
 const api = {
@@ -22,55 +22,94 @@ function App() {
     try{
       const a = await axios.get(url);
       setlistrender(a.data)
-      
     }
     catch (error){
       alert("loi api")
     }
   }
-  
+
+  const debounce = (callback, delay) =>{
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => callback(...args),delay)
+    }
+  }
+
+  const debounceLog = useCallback (
+    debounce(text => console.log(text), 1000),[]
+  );
+
   const onChageName = (e) => {
-    if(typingTimeoutRef.current){
-      clearTimeout(typingTimeoutRef.current)
-    }
-    else{
-      typingTimeoutRef.current = setTimeout(()=>{
-        setGetname(e.target.value)
-        const formValue = {
-          Getname: e.target.value,
-        };
-      },5000)
-    }
+    setGetname(e.target.value)
+    debounceLog(e.target.value)
   }
 
 
   
   const checkwerther = () =>{
-    if(listrender?.main?.temp > 25){
-      alert("hot")
+    if(listrender?.main?.temp > 30 && !listrender?.rain ){
+      return(
+        <div className="all nkm">
+          Nóng không mưa
+        </div>
+      )
     }
-    else{
-      alert("cold")
+    if(listrender?.main?.temp > 30 && listrender?.rain){
+      return(
+        <div className="all ncm">
+          Nóng có mưa          
+        </div>
+      )
+    }
+    if(listrender?.main?.temp >= 20 && listrender?.main?.temp <= 30 && !listrender?.rain){
+      return(
+        <div className="all btkm">
+          Bình thường không mưa
+        </div>
+      )
+    }
+    if(listrender?.main?.temp >= 20 && listrender?.main?.temp <= 30 && listrender?.rain){
+      return(
+        <div className="all btcm">
+          Bình thường có mưa
+        </div>
+      )
+    }
+    if(listrender?.main?.temp <20  && !listrender?.rain){
+      return(
+        <div className="all lkm">
+          Lạnh không mưa
+        </div>
+      )
+    }
+    if(listrender?.main?.temp <20  && listrender?.rain){
+      return(
+        <div className="all lcm">
+          Lạnh có mưa
+        </div>
+      )
     }
   }
   console.log(Getname);
   
 
-  return (
-    <div className="background margin">
-
-      <form action="/action_page.php">
-      <input value={Getname} type="text"  onChange={onChageName}/>
-      <p className='buttom' onClick={lookfor}>Tim kiem</p>
-      {/* <p className='buttom' onClick={checkwerther}>check</p> */}
-      </form>
-        <div className='font'>Nhiệt độ là: { listrender?.main?.temp}<sup>0</sup>C</div>
-        <div className='font'>Tên thành phố {listrender?.name}</div>
-        <div className='font'>Tên quốc gia: {listrender?.sys?.country}</div>
-        {listrender?.rain ? <div>co mua</div> :<div>khong co mua</div>}
-
+  return (       
+    <div>
+      {checkwerther() == null ? <div className='all'>nhap du lieu</div> : checkwerther()}
+      <div className='body'>
+        <div className="margin background ">
+            <input value={Getname} type="text"  onChange={onChageName}/>
+            <p className='buttom' onClick={lookfor}>Tim kiem</p>
+            <div className='font'>Nhiệt độ là: { listrender?.main?.temp}<sup>0</sup>C</div>
+            <div className='font'>Tên thành phố {listrender?.name}</div>
+            <div className='font'>Tên quốc gia: {listrender?.sys?.country}</div>
+            {listrender?.rain ? <div>co mua</div> :<div>khong co mua</div>}
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
